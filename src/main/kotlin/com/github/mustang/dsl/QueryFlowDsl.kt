0c200fb55e68c4d79db73408c4657a472a538565
Mathieu Lalonde <mrlalonde@live.ca>
2019-09-18 09:40:08 -0400

    Added inputs and fixed naming and branch step outputs

--- src/main/kotlin/com/github/mustang/dsl/QueryFlowDsl.kt
+++ src/main/kotlin/com/github/mustang/dsl/QueryFlowDsl.kt
@@ -1,19 +1,32 @@
 package com.github.mustang.dsl
 
 import com.github.mustang.api.*
+import java.lang.IllegalArgumentException
 import java.util.*
+import java.util.stream.Collectors
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class QueryFlowDsl(
     private val nodes: MutableList<WorkflowDocument.Node> = ArrayList(),
     private var started: Boolean = false
 ) {
+    private val dslContext = DslContext()
+    private val inputs:  MutableList<Input> = ArrayList()
+
+    fun input(name: String, init: Input.() -> Unit) : Input {
+        val input = Input(name)
+        input.init()
+        inputs.add(input)
+        return input
+    }
+
     fun start(init: Branch.() -> Unit): Branch {
         if (started) throw IllegalStateException("Already started!")
 
         started = true
 
-        val branch = Branch()
+        val branch = Branch(BranchContext(dslContext))
         branch.init()
         nodes.addAll(branch.build())
         return branch
@@ -21,17 +34,16 @@ class QueryFlowDsl(
 
     fun build(): WorkflowDocument {
         return WorkflowDocument(
-            inputs = listOf(
-                WorkflowDocument.Input(
-                    "Seed",
-                    listOf(WorkflowDocument.Field("Seed", "IP"))
-                )
-            ),
+            inputs = buildInputs(),
             params = emptyList(),
             nodes = this.nodes,
             outputs = setOf("My Query")
         )
     }
+
+    private fun buildInputs(): List<WorkflowDocument.Input> = inputs.stream()
+        .map(Input::toWorkflowDocumentInput)
+        .collect(Collectors.toList())
 }
 
 fun queryFlowDsl(init: QueryFlowDsl.() -> Unit): QueryFlowDsl {
@@ -40,12 +52,23 @@ fun queryFlowDsl(init: QueryFlowDsl.() -> Unit): QueryFlowDsl {
     return dsl
 }
 
-class Branch {
-    private val stackState: Stack<String> = Stack()
+class Input(private val name: String) {
+    private val fields: MutableList<WorkflowDocument.Field> = ArrayList()
+
+    fun withField(fieldName: String, fieldType: String) {
+        fields.add(WorkflowDocument.Field(fieldName, fieldType))
+    }
+
+    fun toWorkflowDocumentInput() : WorkflowDocument.Input {
+        return WorkflowDocument.Input(name, fields)
+    }
+}
+
+class Branch(private val branchContext: BranchContext) {
     private val nodes: MutableList<WorkflowDocument.Node> = ArrayList()
 
     fun query(id: String, init: Query.() -> Unit): Query {
-        val query = Query(id = id, previous = previous())
+        val query = Query(id = id, branchContext = branchContext)
 
         query.init()
         nodes.add(query.toNode())
@@ -53,40 +76,78 @@ class Branch {
     }
 
     fun call(id: String, init: ServiceCall.() -> Unit): ServiceCall {
-        val serviceCall = ServiceCall(id)
+        val serviceCall = ServiceCall(id, branchContext)
         serviceCall.init()
         nodes.add(serviceCall.toNode())
         return serviceCall
     }
 
-    private fun previous() = if (stackState.isEmpty()) "" else stackState.pop()
-
     fun build(): List<WorkflowDocument.Node> = nodes
+}
+
+class DslContext {
+    private val registeredNames: MutableMap<String, Int> = HashMap()
+
+    fun newName(candidateName: String) : String {
+        val numberOfCopies = registeredNames.getOrDefault(candidateName, 0)
+        registeredNames[candidateName] = numberOfCopies + 1
+        return if (numberOfCopies == 0) candidateName else "$candidateName-$numberOfCopies"
+    }
+}
+
+class BranchContext(private val dslContext: DslContext) {
+    private val stackState: Stack<String> = Stack()
+
+    fun registerNewName(candidateName: String) = dslContext.newName(candidateName)
+    
+    fun pushOutput(output: String) {
+        stackState.push(output)
+    }
+
+    fun previousOutput(): String = if (stackState.isEmpty()) "" else stackState.pop()
 }
 
 interface NodeFactory {
     fun toNode(): WorkflowDocument.Node
 }
 
-data class Query(val id: String, var seeds: Seeds = CurrentSeeds, val previous: String) : NodeFactory {
-    override fun toNode(): WorkflowDocument.Node {
-        return WorkflowDocument.Node(
-            "queryService", id,
-            mapOf(
-                SEEDS_PARAM_NAME to SeedParam(seeds),
-                "QUERY_ID" to StringParam(id)),
-            inputs = if (previous.isEmpty()) emptySet() else setOf(previous)
-        )
+abstract class BranchStep(val serviceId: String, private val branchContext: BranchContext): NodeFactory{
+    private var inputName: String = ""
+    protected var name: String = ""
+    set(value) {
+        field = branchContext.registerNewName(value)
+    }
+    val params: MutableMap<String, ParamValue> = HashMap()
+
+    final override fun toNode(): WorkflowDocument.Node {
+        postInit()
+        if (inputName.isEmpty())
+            inputName = branchContext.previousOutput()
+
+        val inputs = if (inputName.isEmpty()) emptySet() else setOf(inputName)
+
+        branchContext.pushOutput(name)
+        return WorkflowDocument.Node(serviceId, name, params, inputs)
+    }
+
+    protected abstract fun postInit()
+}
+
+data class Query(val id: String, var seeds: Seeds = CurrentSeeds,
+                 private val branchContext: BranchContext) : BranchStep("queryService", branchContext) {
+    override fun postInit() {
+        params[SEEDS_PARAM_NAME] = SeedParam(seeds)
+        params["QUERY_ID"] = StringParam(id)
+        super.name = id
     }
 }
 
 data class ServiceCall(
     val id: String,
-    var inputs: Set<String> = emptySet(),
-    var parameters: Map<String, ParamValue> = emptyMap()
-) : NodeFactory {
-    override fun toNode(): WorkflowDocument.Node {
-        return WorkflowDocument.Node(id, id, parameters, inputs)
+    val branchContext: BranchContext
+) : BranchStep(serviceId = id, branchContext = branchContext) {
+    override fun postInit() {
+        if (name.isEmpty()) name = serviceId
     }
 
 }
--- src/test/kotlin/com/github/mustang/dsl/QueryFlowDslTest.kt
+++ src/test/kotlin/com/github/mustang/dsl/QueryFlowDslTest.kt
@@ -1,5 +1,6 @@
 package com.github.mustang.dsl
 
+import com.github.mustang.api.ResultColumnSeeds
 import com.github.mustang.api.StringParam
 import com.github.mustang.api.WorkflowDocument
 import com.github.mustang.api.newObjectMapper
@@ -8,18 +9,17 @@ import kotlin.test.assertEquals
 
 class QueryFlowDslTest {
     @Test
-    fun test() {
+    fun workflowOneBranch() {
         val dsl = queryFlowDsl {
-            start() {
+            input("Seed") {
+                withField("Seed", "IP")
+            }
+            start {
                 query(id = "geoLocateIP") {
                 }
                 call(id = "sql-service") {
-                    parameters = mapOf(
-                        "sql" to
-                                StringParam("SELECT IP, COUNTRY, COUNT(1) AS TOTAL GROUP BY (IP, COUNTRY)")
-                    )
+                    params["sql"] = StringParam("SELECT IP, COUNTRY, COUNT(1) AS TOTAL GROUP BY (IP, COUNTRY)")
                 }
-
             }
         }
 
@@ -59,7 +59,7 @@ class QueryFlowDslTest {
         "type" : "STRING"
       }
     },
-    "inputs" : [ ]
+    "inputs" : [ "geoLocateIP" ]
   } ],
   "outputs" : [ "My Query" ]
 }"""
@@ -73,5 +73,98 @@ class QueryFlowDslTest {
             start { }
             start { }
         }
+    }
+
+    @Test
+    fun workflowNameClash() {
+        val dsl = queryFlowDsl {
+            start {
+                input("Seed") {
+                    withField("Seed", "IP")
+                    withField("Other Field", "STRING")
+                }
+                query(id = "findFriends") {
+                }
+                query(id = "findFriends") {
+                    seeds = ResultColumnSeeds(resultName = "findFriends", columnNames = listOf("Friend"))
+                }
+                query(id = "findFriends") {
+                    seeds = ResultColumnSeeds(resultName = "findFriends-1", columnNames = listOf("Friend"))
+                }
+            }
+        }
+
+        val document = dsl.build()
+        val expectedString = """
+ {
+  "inputs" : [ {
+    "name" : "Seed",
+    "fields" : [ {
+      "name" : "Seed",
+      "type" : "IP"
+    },
+     {
+       "name" : "Other Field",
+       "type" : "STRING"
+     }]
+  } ],
+  "params" : [ ],
+  "nodes" : [ {
+    "serviceId" : "queryService",
+    "name" : "findFriends",
+    "params" : {
+      "SEEDS" : {
+        "seeds" : {
+          "type" : "CURRENT"
+        },
+        "type" : "SEEDS"
+      },
+      "QUERY_ID" : {
+        "value" : "findFriends",
+        "type" : "STRING"
+      }
+    },
+    "inputs" : [ ]
+  }, {
+    "serviceId" : "queryService",
+    "name" : "findFriends-1",
+    "params" : {
+      "SEEDS" : {
+        "seeds" : {
+          "resultName" : "findFriends",
+          "columnNames" : [ "Friend" ],
+          "type" : "RESULT_COLUMNS"
+        },
+        "type" : "SEEDS"
+      },
+      "QUERY_ID" : {
+        "value" : "findFriends",
+        "type" : "STRING"
+      }
+    },
+    "inputs" : [ "findFriends" ]
+  }, {
+    "serviceId" : "queryService",
+    "name" : "findFriends-2",
+    "params" : {
+      "SEEDS" : {
+        "seeds" : {
+          "resultName" : "findFriends-1",
+          "columnNames" : [ "Friend" ],
+          "type" : "RESULT_COLUMNS"
+        },
+        "type" : "SEEDS"
+      },
+      "QUERY_ID" : {
+        "value" : "findFriends",
+        "type" : "STRING"
+      }
+    },
+    "inputs" : [ "findFriends-1" ]
+  } ],
+  "outputs" : [ "My Query" ]
+} """
+        assertEquals(newObjectMapper().readValue(expectedString, WorkflowDocument::class.java), document,
+            " $expectedString \n\nmismatch\n\n ${newObjectMapper().writeValueAsString(document)}")
     }
 }
